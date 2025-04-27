dac4570b236c52c6409fc435b183eb1902b5f287
amit07 <kolpeamit6@gmail.com>
2025-04-27 11:37:57 +0530

    Runtime exception

---
+++ Exception.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Exception{
+    public static void main(String[] args){
+        int k;
+        Scanner sc = new Scanner(System.in);
+        k= sc.nextInt();
+
+        System.out.println("The divide by 1000 is:"+1000/k);
+        
+    }
+}
