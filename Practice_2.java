703b23d9deedb566c9f11d04ce198c051cb00aeb
amit07 <kolpeamit6@gmail.com>
2025-05-03 21:59:24 +0530

    practice

---
+++ Practice_2.java
@@ -0,0 +1,16 @@
+
+import java.util.HashSet;
+
+
+
+
+class Practice_2{
+    public static void main(String[] args) {
+        
+        HashSet<Integer> s =  new HashSet<>();
+        s.add(89);
+        s.add(100);
+        s.add(30);
+        System.out.println(s);
+    }
+} 
---
+++ praactice_3.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+
+
+class Practice_3{
+    public static void main(String[] args) {
+        
+        Date d  = new Date();
+        System.out.println(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+
+    }
+}
