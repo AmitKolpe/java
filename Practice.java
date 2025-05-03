e54e65443016098c04b165c0bf04d22dfc03af63
amit07 <kolpeamit6@gmail.com>
2025-05-03 21:58:59 +0530

    practice

---
+++ Practice.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+
+
+
+class Practice{
+    public static void main(String[] args) {
+        
+        ArrayList ar = new ArrayList();
+        ar.add("john");
+        ar.add("finch");
+        ar.add("smith");
+        ar.add("pat");
+        ar.add("root");
+        ar.add("tom");
+        ar.add("mmax");
+        ar.add("raguu");
+        ar.add("ansh");
+        ar.add("vithuu");
+        for (Object o : ar) {
+            System.out.println(o);
+            
+        }
+    }
+}
