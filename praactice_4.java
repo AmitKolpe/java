fb053ad6ec4ac3b320a86bc1f6d087fcc136ffbb
amit07 <kolpeamit6@gmail.com>
2025-05-04 22:37:25 +0530

    practice time

---
+++ praactice_4.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+
+
+class Practice_4{
+    public static void main(String[] args) {
+        
+        Date d  = new Date();
+        System.out.println(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+
+    }
+}
