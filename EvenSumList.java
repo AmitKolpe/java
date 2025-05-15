ce88d01fdc72b21df1ff4c81a7f4979ee4223e82
amit07 <kolpeamit6@gmail.com>
2025-05-15 23:10:07 +0530

    list sum

---
+++ EvenSumList.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenSumList {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(4, 9, 12, 7, 6);
+        int sum = 0;
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                sum += num;
+            }
+        }
+
+        System.out.println("Sum of even numbers in the list: " + sum);
+    }
+}
