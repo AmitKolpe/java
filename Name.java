338ca1a1751e78e6ac1909e460f633bed4aa215f
amit07 <kolpeamit6@gmail.com>
2025-05-25 23:13:43 +0530

    Birds

---
+++ Name.java
@@ -0,0 +1,10 @@
+public class Name {
+    public static void main(String[] args) {
+        String[] birds = {"Sparrow", "Parrot", "Peacock", "Eagle", "Flamingo", "Owl", "Penguin", "Kingfisher"};
+
+        System.out.println("List of Bird Names:");
+        for (String bird : birds) {
+            System.out.println("- " + bird);
+        }
+    }
+}
