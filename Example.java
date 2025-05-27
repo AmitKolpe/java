c873a67eea2a6ff4fa79c82332d20414c86f5d51
amit07 <kolpeamit6@gmail.com>
2025-05-28 00:41:30 +0530

    If-Else

---
+++ Example.java
@@ -0,0 +1,13 @@
+public class Example{
+    public static void main(String[] args) {
+        int number = 25;
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
