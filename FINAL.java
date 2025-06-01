c786cb0961bce67a3de36e36bb2756b2b07a0a2e
amit07 <kolpeamit6@gmail.com>
2025-06-01 17:41:52 +0530

    FINAL KEYWORD

---
+++ FINAL.java
@@ -0,0 +1,11 @@
+class FINAL {
+    public static final void number() {
+        int var1 = 100;
+        int var2 = 200; 
+        System.out.println("var1: " + var1 + ", var2: " + var2);
+    }
+
+    public static void main(String[] args) {
+        number();
+    }
+}
