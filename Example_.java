cabf10d4a09220118dd7ff561442795d25b1917d
amit07 <kolpeamit6@gmail.com>
2025-06-02 21:29:21 +0530

    String

---
+++ Example_.java
@@ -0,0 +1,7 @@
+public class Example_ {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        int len = str.length();
+        System.out.println("Length = " + len);
+    }
+}
